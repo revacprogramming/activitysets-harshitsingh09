bbd4c6240680b3eff2e8bbb9fb4c0526faa7f616
Harshit Singh <89565061+harshitsingh09@users.noreply.github.com>
2022-05-13 04:19:50 +0000

    complete p03

--- ActivitySet#1/p02.py
+++ ActivitySet#1/p02.py
@@ -1,4 +1,5 @@
 # Why Program
-#Write a program that uses a print statement to say 'hello world' as shown in 'Desired Output'.
+
+# Write a program that uses a print statement to say 'hello world' as shown in 'Desired Output'.
 
 print("hello world")
