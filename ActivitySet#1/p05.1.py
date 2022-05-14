c956c389d3670cc7476ed2e7c90d3a60e2b7aee3
Harshit Singh <89565061+harshitsingh09@users.noreply.github.com>
2022-05-14 04:46:23 +0000

    complete  p04

--- ActivitySet#1/p05.1.py
+++ ActivitySet#1/p05.1.py
@@ -1,5 +1,18 @@
-#
+# Loops & Iterators
 
 # Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below. Convert the extracted value to a floating point number and print it out.
 
-text = "X-DSPAM-Confidence:    0.8475"
+largest = None
+smallest = None
+
+while True:
+    num = input("Enter a number? ")
+
+    if num == "done":
+        break
+
+    # ...
+
+    print(num)
+
+print("Maximum", largest)
