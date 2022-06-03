727a24094b70c506b420d78321c0bbf8ef39e38a
Harshit Singh <89565061+harshitsingh09@users.noreply.github.com>
2022-06-03 20:27:31 +0000

    working p11

--- ActivitySet#1/p11.py
+++ ActivitySet#1/p11.py
@@ -4,5 +4,12 @@
 The basic outline of this problem is to read the file, look for integers using the re.findall(), looking for a regular expression of '[0-9]+' and then converting the extracted strings to integers and summing up the integers.
 '''
 
-count = 0
-reg = open(regex.txt)
+import re
+fh = open("regex_sum_269097.txt")
+sum = 0
+for line in fh:
+    num = re.findall('[0-9]+',line)
+    length = len(num)
+    for i in range(length):
+        sum += int(num[i])
+print(sum)
