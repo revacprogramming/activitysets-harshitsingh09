5f085188758063e35ef05328099995bfe31b9be7
harshitsingh09 <harshit.arvind@gmail.com>
2022-07-09 23:29:48 +0530

    complete p13.3

---
+++ ActivitySet#1/p13.3.py
@@ -0,0 +1,40 @@
+# Using Web Services
+# https://www.py4e.com/tools/python-data/?PHPSESSID=d5904940148fb13c65ff498fada145d5
+
+'''
+Following Links in Python
+
+In this assignment you will write a Python program that expands on http://www.py4e.com/code3/urllinks.py. The program will use urllib to read the HTML from the data files below, extract the href= vaues from the anchor tags, scan for a tag that is in a particular position relative to the first name in the list, follow that link and repeat the process a number of times and report the last name you find.
+
+We provide two files for this assignment. One is a sample file where we give you the name for your testing and the other is the actual data you need to process for the assignment
+
+Sample problem: Start at http://py4e-data.dr-chuck.net/known_by_Fikret.html
+Find the link at position 3 (the first name is 1). Follow that link. Repeat this process 4 times. The answer is the last name that you retrieve.
+Sequence of names: Fikret Montgomery Mhairade Butchi Anayah
+Last name in sequence: Anayah
+Actual problem: Start at: http://py4e-data.dr-chuck.net/known_by_Kaelin.html
+Find the link at position 18 (the first name is 1). Follow that link. Repeat this process 7 times. The answer is the last name that you retrieve.
+Hint: The first character of the name of the last page that you will load is: J
+Strategy
+The web pages tweak the height between the links and hide the page after a few seconds to make it difficult for you to do the assignment without writing a Python program. But frankly with a little effort and patience you can overcome these attempts to make it a little harder to complete the assignment without writing a Python program. But that is not the point. The point is to write a clever Python program to solve the program.
+'''
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+url = input('Enter URL:')
+count = int(input('Enter count:'))
+position = int(input('Enter position:'))-1
+html = urlopen(url).read()
+
+soup = BeautifulSoup(html,"html.parser")
+href = soup('a')
+#print href
+
+for i in range(count):
+    link = href[position].get('href', None)
+    print (href[position].contents[0])
+    html = urlopen(link).read()
+    soup = BeautifulSoup(html,"html.parser")
+    href = soup('a')
